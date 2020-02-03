8368b14ee6001ae0aad8d2cb05b33af17f847cce
Nydragon00 <6582485n@gmail.com>
2020-02-03 14:53:21 +0100

    TDD all cases

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -2,13 +2,21 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
+from texttest_fixture import List
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+        lst = List()
+        sel_item = lst.itemsl()
+        # print(len(sel_item))
+        # items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(sel_item)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        for i in range(len(sel_item)):
+            try:
+                self.assertEqual(20, sel_item[i].sell_in)
+            except AssertionError:
+                print("Error, {}".format(sel_item[i].sell_in))
 
 if __name__ == '__main__':
     unittest.main()
