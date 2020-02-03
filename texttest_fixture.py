9e91e8f95e43e2ee43f62368953a2539e10e1c7e
Nydragon00 <6582485n@gmail.com>
2020-02-03 15:00:29 +0100

    cleanup

--- texttest_fixture.py
+++ texttest_fixture.py
@@ -3,36 +3,27 @@ from __future__ import print_function
 
 from gilded_rose import *
 
+items = [
+Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+Item(name="Aged Brie", sell_in=2, quality=0),
+Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+]
+
 class List:
     def __init__(self):
         return
 
     def itemsl(self):
-        items = [
-                 Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-                 Item(name="Aged Brie", sell_in=2, quality=0),
-                 Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-                 Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-                 Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-                 Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-                 Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-                 Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-                 Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-                ]
         return items
+
 if __name__ == "__main__":
     print ("OMGHAI!")
-    items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
-            ]
 
     days = 2
     import sys
